0c3c0777ef92a49d8bd797f151b32fcfb35605c5
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-03-07 14:01:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// ROUTES
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+});
